52a16fe2064bf0d829648a71022033c106fbcea8
LAPTOP-4BRP3BD3\panagiotis <patakospap@gmail.com>
2017-05-22 14:23:09 +0300

    add a contribution

--- User.java
+++ User.java
@@ -1,10 +1,11 @@
 
 public class User {
-
+private String name;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
 		System.out.println("test");
+		
 		
 		
 	}
