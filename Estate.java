f81a1338661c0bc31f66fa50a995d01bb5156643
chris alex <chrisalex@MacBook-Pro.local>
2017-05-22 15:09:26 +0300

    EGINAN TA ESTATE KAI VEHICLE

---
+++ Estate.java
@@ -0,0 +1,23 @@
+
+public class Estate {
+
+	private String address;
+	private int square_meters;
+	
+	
+	
+	public Estate(String address, int square_meters) {
+		super();
+		this.address = address;
+		this.square_meters = square_meters;
+	}
+	
+	public double calculate_est_charge(){
+		// NA VROUME XREOSEIS ANA TETRAGWNIKA METRA//
+		
+		double charge = 0 ;
+		
+		return charge;
+	}
+	
+}
---
+++ Vehicle.java
@@ -0,0 +1,26 @@
+
+public class Vehicle {
+
+	private int cc;
+	private int year;
+	private String veh_id;
+	private boolean taxed;
+	
+	
+	public Vehicle(int cc, int year, String veh_id, boolean taxed) {
+		super();
+		this.cc = cc;
+		this.year = year;
+		this.veh_id = veh_id;
+		this.taxed = taxed;
+	}
+	
+	public double calculate_veh_charge(){
+		// NA VROUME TIS XREOSEIS ANA KUVIKO //
+		double charge = 0;
+		
+		
+		return charge;
+	}
+	
+}
