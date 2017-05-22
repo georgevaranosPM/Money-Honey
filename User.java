7379272ce8ba805bac50f77c86d72a6a93e1c1f7
LAPTOP-4BRP3BD3\panagiotis <patakospap@gmail.com>
2017-05-22 14:56:14 +0300

    add Contributions at class User

--- User.java
+++ User.java
@@ -1,5 +1,27 @@
 
 public class User {
+
+	private String username;
+	private String password;
+	private String birthday;
+	
+	private ArrayList<Income> incomes;
+	private ArrayList<Expense> expenses;
+	
+	
+	public User(String username, String password) {
+		
+		this.username = username;
+		this.password = password;
+	}
+	
+	public void add_Income(Income aIncome){
+		
+	}
+	public void add_Expense(Expense aExpense){
+		
+	}
+	
 	
 
 
