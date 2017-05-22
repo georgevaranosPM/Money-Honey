b6f8cadb3aaf4e1861206ba0d747c5527e147dba
chris alex <chrisalex@MacBook-Pro.local>
2017-05-22 14:56:50 +0300

    kana tin income MIN TIN PEIRAKSETE

--- Income.java
+++ Income.java
@@ -1,6 +1,20 @@
 
+
 public class Income {
 
-	private int kati;
+	private String in_tag;
+	private double in_amount;
+	private boolean monthly;
+	private boolean taxed;
 	
-}
+
+
+	public Income(String in_tag, double in_amount, boolean monthly, boolean taxed) {
+		super();
+		this.in_tag = in_tag;
+		this.in_amount = in_amount;
+		this.monthly = monthly;
+		this.taxed = taxed;
+	}
+
+} 
