71ac4fd155f10878f53114e3a8e544f41f25372f
Γιώργος Βαράνος <dai16104@uom.edu.gr>
2017-05-22 14:53:34 +0300

    Adding Expense.java

---
+++ Expense.java
@@ -0,0 +1,26 @@
+
+public class Expense {
+	
+	private String ex_tag;
+	private double ex_amount;
+	
+	
+	public Expense(String ex_tag, double ex_amount) {
+		super();
+		this.ex_tag = ex_tag;
+		this.ex_amount = ex_amount;
+	}
+
+	public boolean check_Limit(double sum, double limit) {
+		
+		boolean passed = false;
+		
+		return passed;
+	}
+	
+	public void add_newDefault(Expense newExp) {
+
+	}
+	
+}
+
