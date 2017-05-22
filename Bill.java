c3f426b58e5761a8071794f55f683fd72099ab74
Γιώργος Βαράνος <dai16104@uom.edu.gr>
2017-05-22 15:10:00 +0300

    Updating Bill and Consumable

--- Bill.java
+++ Bill.java
@@ -2,10 +2,12 @@ import java.util.ArrayList;
 
 public class Bill extends Expense{
 
+	private ArrayList<String> default_Bills;
+	
 	public Bill(String ex_tag, double ex_amount) {
 		super(ex_tag, ex_amount);
 	}
 	
-	private ArrayList<Bill> default_Bills;
+	
 	
 }
--- Consumable.java
+++ Consumable.java
@@ -2,9 +2,12 @@ import java.util.ArrayList;
 
 public class Consumable extends Expense {
 
+	private ArrayList<String> default_Consumables;
+	
 	public Consumable(String ex_tag, double ex_amount) {
 		super(ex_tag, ex_amount);
 	}
 	
-	private ArrayList<Consumable> default_Consumables;
+	
+	
 }
